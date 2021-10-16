7026aa3b557b0006ef9b8d0beb1e59ec15636fe2
MikuroXina <ryosukadnak@gmail.com>
2021-10-16 14:13:33 +0900

    feat: Add pin command

--- bin/server.ts
+++ bin/server.ts
@@ -1,11 +1,48 @@
+import {
+  ApplicationCommandType,
+  Interaction,
+  InteractionResponseType,
+  InteractionType,
+} from "src/types";
 import { createHandler } from "../src/handler";
 
 declare const APPLICATION_ID: string;
 declare const APPLICATION_SECRET: string;
 declare const PUBLIC_KEY: string;
 
+const errorResponse = {
+  type: InteractionResponseType.ChannelMessageWithSource,
+  data: {
+    content: "ピン留めできないみたいです…",
+  },
+};
+
 const handler = createHandler({
-  commands: [],
+  commands: [
+    [
+      {
+        type: ApplicationCommandType.Message,
+        name: "Pin this message",
+      },
+      async (interaction: Interaction) => {
+        if (interaction.type !== InteractionType.ApplicationCommand) {
+          return errorResponse;
+        }
+        const messages = interaction.data.resolved?.messages;
+        if (messages === undefined) {
+          return errorResponse;
+        }
+        const message = Object.values(messages)[0];
+        // TODO: transport message to the webhook
+        return {
+          type: InteractionResponseType.ChannelMessageWithSource,
+          data: {
+            content: "ピン留めしておきます！",
+          },
+        };
+      },
+    ],
+  ],
   applicationId: APPLICATION_ID,
   applicationSecret: APPLICATION_SECRET,
   publicKey: PUBLIC_KEY,
