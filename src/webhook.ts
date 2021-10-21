9a89e577e063b975fdfad91692248098711c95b2
MikuroXina <ryosukadnak@gmail.com>
2021-10-21 19:50:30 +0900

    Add webhook

---
+++ src/webhook.ts
@@ -0,0 +1,23 @@
+const ENDPOINT = "https://discord.com/api/v8";
+
+export interface WebhookOptions {
+  webhookId: string;
+  webhookToken: string;
+}
+
+export const webhook =
+  ({ webhookId, webhookToken }: WebhookOptions) =>
+  async (message: unknown): Promise<void> => {
+    const res = await fetch(
+      [ENDPOINT, "webhooks", webhookId, webhookToken].join("/"),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(message),
+      },
+    );
+    if (!res.ok) {
+      console.error(await res.text());
+    }
+  };
