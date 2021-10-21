97f0ea746f7e28134c1cfd80451e38fb627b6074
MikuroXina <ryosukadnak@gmail.com>
2021-10-21 20:07:11 +0900

    Fix method on fetch to webhook

--- src/webhook.ts
+++ src/webhook.ts
@@ -14,6 +14,7 @@ export const webhook =
         headers: {
           "Content-Type": "application/json",
         },
+        method: "POST",
         body: JSON.stringify(message),
       },
     );
