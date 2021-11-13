c83325a5cf5eec6b075e1bba58637fda1d867afa
MikuroXina <ryosukadnak@gmail.com>
2021-11-14 02:26:54 +0900

    Improve webhook type

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,25 +1,53 @@
-import { ENDPOINT, PartialMessage } from "./types";
+import { Attachment, ENDPOINT, Embed, Snowflake } from "./types";
 
 export interface WebhookOptions {
   webhookId: string;
   webhookToken: string;
 }
 
+export type WebhookMessage = (
+  | {
+      content: string;
+      embeds?: Embed[];
+      files?: Blob[];
+    }
+  | {
+      content?: string;
+      embeds: Embed[];
+      files?: Blob[];
+    }
+  | {
+      content?: string;
+      embeds?: Embed[];
+      files: Blob[];
+    }
+) &
+  Partial<{
+    username: string;
+    // eslint-disable-next-line camelcase
+    avatar_url: string;
+    tts: boolean;
+    // eslint-disable-next-line camelcase
+    allowed_mentions: Snowflake[];
+    components: unknown[];
+    attachments: Attachment[];
+  }>;
+
 export const webhook =
   ({ webhookId, webhookToken }: WebhookOptions) =>
-  async (
-    partialMessage: PartialMessage,
-    files: readonly Blob[],
-  ): Promise<void> => {
+  async (partialMessage: Readonly<WebhookMessage>): Promise<void> => {
     const message = { ...partialMessage };
     const form = new FormData();
 
-    for (let idx = 0; idx < files.length; idx += 1) {
-      const file = files[idx];
-      form.append(`files[${idx}]`, file, `${idx}`);
-      message.attachments[idx].id = `${idx}`;
-      message.attachments[idx].filename = `${idx}`;
+    if (message.files) {
+      for (let idx = 0; idx < message.files.length; idx += 1) {
+        const file = message.files[idx];
+        const filename =
+          message.attachments && message.attachments[idx].filename;
+        form.append(`files[${idx}]`, file, filename);
+      }
     }
+    Reflect.deleteProperty(message, "files");
     form.append("payload_json", JSON.stringify(message));
 
     for (const [key, value] of form.entries()) {
