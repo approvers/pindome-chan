8d63a54df0e6cd5bfa62634d8ad9c68d068db1f0
Mikuroさいな <ryosukadnak@gmail.com>
2023-02-02 15:31:10 +0900

    feat: 画像のピン留め対応 (#38)
    * Use FormData
    
    * Fix key of form data
    
    * Specify User-Agent
    
    * Debug message
    
    * Revert "Fix key of form data"
    
    This reverts commit 664d7c2f5af61f6455e597462b2074db05e72e19.
    
    * Fix logging
    
    * Fix to trim content
    
    * Remove spread
    
    * Add message_reference
    
    * Use blob to append json
    
    * Split content data
    
    * Remove Content-Type header
    
    * Handle webhook error
    
    * Remove content fallback
    
    * Fix to send string
    
    * Fix to spread message
    
    * Recover content field

--- src/commands.ts
+++ src/commands.ts
@@ -7,33 +7,38 @@ import {
   InteractionType,
 } from "./types.ts";
 
-const errorResponse = {
+const errorResponse = (reason: string) => ({
   type: InteractionResponseType.ChannelMessageWithSource,
   data: {
-    content: "ピン留めできないみたいです…",
+    content: `${reason}、ピン留めできないみたいです…`,
   },
-};
+});
 
 export interface WebhookOptions {
   webhookId: string;
   webhookToken: string;
 }
 
+const USER_AGENT =
+  "pindome-chan Bot (https://github.com/approvers/pindome-chan)";
+
 const sendWebhook = async (
-  message: unknown,
+  message: FormData,
   { webhookId, webhookToken }: WebhookOptions,
 ): Promise<void> => {
   const res = await fetch(
     [ENDPOINT, "webhooks", webhookId, webhookToken].join("/"),
     {
       headers: {
-        "Content-Type": "application/json",
+        "User-Agent": USER_AGENT,
       },
       method: "POST",
-      body: JSON.stringify(message),
+      body: message,
     },
   );
-  console.log(await res.text());
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
 };
 
 export const makeCommands = (options: WebhookOptions): InteractionHandlers => [
@@ -44,20 +49,46 @@ export const makeCommands = (options: WebhookOptions): InteractionHandlers => [
     },
     async (interaction: Interaction) => {
       if (interaction.type !== InteractionType.ApplicationCommand) {
-        return errorResponse;
+        return errorResponse("コマンドの種類が違うから");
       }
       const messages = interaction.data.resolved?.messages;
       if (messages === undefined) {
-        return errorResponse;
+        return errorResponse("間に合わなかったから");
       }
       const [message] = Object.values(messages);
-      message.attachments = message.attachments.filter(
-        (attachment) => attachment.ephemeral === false,
+      const form = new FormData();
+      form.append(
+        "payload_json",
+        JSON.stringify({
+          ...message,
+          content: `${message.content}\nby ${message.author.username}`.trim(),
+          allowed_mentions: false,
+          message_reference: {
+            message_id: message.id,
+          },
+          attachments: message.attachments.map((attachment, index) => ({
+            id: index,
+            filename: attachment.filename,
+          })),
+        }),
       );
-      await sendWebhook({
-        ...message,
-        content: `${message.content}\nby ${message.author.username}`,
-      }, options);
+      await Promise.all(message.attachments.map(async (attachment, index) => {
+        const res = await fetch(attachment.url);
+        const blob = await res.blob();
+
+        const UPLOAD_SIZE_LIMIT = 8 * 1024 * 1024;
+        if (UPLOAD_SIZE_LIMIT < blob.size) {
+          return errorResponse("アップロード上限を超えているから");
+        }
+        form.append(`files[${index}]`, blob, attachment.filename);
+      }));
+      try {
+        await sendWebhook(form, options);
+      } catch (err) {
+        console.error(err);
+        return errorResponse("ウェブフックの送信に失敗したから");
+      }
+
       let content = "ピン留めしましたっ！";
       if (message.content.length !== 0) {
         content += "\n";
